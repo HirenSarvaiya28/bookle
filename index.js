ba625c2ef60b41b4a914b61a121b999b87f68b48
TeamF4L <ogitachi@gmail.com>
2025-04-21 10:27:11 +0530

    core

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const port = process.env.PORT || 3000;
 
 const db = require("./config/db")
 
+const cors = require('cors.js');
+
+app.use(cors());
+
 const path = require('path')
 
 app.use(express.urlencoded());
 
 app.use('/', require('./Routes/indexRoutes'))
 
-app.listen(port, (err)=>{
-    if(err){
+app.listen(port, (err) => {
+    if (err) {
         console.log(err);
         return false;
     }
